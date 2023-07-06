105b921fedbf8ebae2afe59e9f2be795305fd782
junyjeon <jjy8479@gmail.com>
2023-07-06 22:14:28 +0900

    1

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,71 +1,50 @@
-//
-// Created by jjy84 on 2023-05-23(023).
-//
-#include "PhoneBook.h"
+#include "PhoneBook.hpp"
 
-void	clear()
-{
-
-}
-
-void	add()
-{
-	std::cout << "Enter: new contact information\n";
-	std::cout << "first_name: ";
-	std::cin >> first_name;
-	std::cout << "last_name: ";
-	std::cin >> last_name;
-	std::cout << "nickname: ";
-	std::cin >> nickname;
-	std::cout << "phone_number: ";
-	std::cin >> phone_number;
-	std::cout << "darkest_secret: ";
-	std::cin >> darkest_secret;
-}
-
-void	search()
-{
+void	PhoneBook::show_all(){
 	std::cout << std::right;
 	std::cout << std::setw(10) << "index" << '|';
 	std::cout << std::setw(10) << "first name" << '|';
 	std::cout << std::setw(10) << "last name" << '|';
-	std::cout << std::setw(10) << "nickname" << std::endl;
-	std::cout << "Enter User index\n";
+	std::cout << std::setw(10) << "nickname" << '|' << std::endl;
+	int i = -1;
+	while (++i < 8)
+		this->contact[i].show_all(i);
 }
 
+void	PhoneBook::search(){
+	this->show_all();
+	std::cout << "Enter: The index of the entry to display" << std::endl;
+	std::string str;
+	int i;
+	std::getline(std::cin, str);
+	i = atoi(str.c_str());
+	if (1 <= i && i <= 8)
+		this->contact[i - 1].display();
+	else
+		std::cout << "Input error: invalid input" << std::endl;
+}
+
+void	PhoneBook::add(){
+	this->contact[idx % 8].add();
+	this->idx++;
+}
 
 int    main(void)
 {
-	PhoneBook	pb[8];
+	PhoneBook	pb;
 	std::string cmd;
 
-    while (!cin.eof()) {
-        std::cin >> cmd;
-        if (cmd.compare("EXIT") == 0){
-            //	프로그램 종료. 연락처 클리어.
-            pb[i].exit();
-			return (0);
-        }
-		else if (cmd.compare("ADD") == 0) {
-			/*	cin으로 한 필드씩 first_name, last_name, nickname, phone_number, darkest_secret 입력
-				요구사항 하나는 영상 봐야댐
-			*/
-			pb[i].add();
-		}
-		else if (cmd.compare("SEARCH") == 0){
-		/*	존재하는 연락처를 index, first_name, last_name, nickname 열로 보여줌
-			너비 10문자 가로정렬 |문자로 구분
-			if 너비보자 긴 문자열은 잘림 마지막 문자는
-		*/
-			pb[i].search();
-		}
-		else if (!std::cin.eof())
+    while (!std::cin.eof()) {
+		std::cout << "Enter the command: " << std::endl;
+        std::getline(std::cin, cmd);
+        if (cmd.compare("EXIT") == 0)
+			break ;
+		else if (cmd.compare("ADD") == 0)
+			pb.add();
+		else if (cmd.compare("SEARCH") == 0)
+			pb.search();
 		else
-			//	버려짐 discarded.
 			continue ;
-
 	}
 	return (0);
 }
-//flush	Flush stream buffer (function)
-//internal	Adjust field by inserting characters at an internal position (function)
